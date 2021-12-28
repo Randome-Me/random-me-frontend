c480b92240a001f01ec1245ef59c099a80e21810
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-28 20:40:59 +0700

    💄 title and desc for /login

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,24 +3,35 @@ import LoginForm from "components/pages/login/LoginForm"
 import LoginSocialMedia from "components/pages/login/LoginSocialMedia"
 import ContinueAsGuest from "components/pages/login/ContinueAsGuest"
 import PageBackground from "components/common/PageBackground"
+import Head from "next/head"
 
 export default function Login() {
   return (
-    <PageBackground src="/images/bg-login-register.svg">
-      <div className="flex min-h-screen justify-center items-center">
-        <h1 className="sr-only">Login</h1>
-        <Glass className="p-[2rem] space-y-4 md:space-y-10 xs:p-10 sm:p-14">
-          <strong
-            className="text-black text-4xl sm:text-5xl font-extrabold
-          flex justify-center"
-          >
-            Random Me.
-          </strong>
-          <LoginForm />
-          <LoginSocialMedia />
-          <ContinueAsGuest />
-        </Glass>
-      </div>
-    </PageBackground>
+    <>
+      <Head>
+        <title>Login | Random Me</title>
+        <meta
+          name="description"
+          content="Login to Random Me or continue as guest."
+        />
+      </Head>
+
+      <PageBackground src="/images/bg-login-register.svg">
+        <div className="flex min-h-screen justify-center items-center">
+          <h1 className="sr-only">Login</h1>
+          <Glass className="p-[2rem] space-y-4 md:space-y-10 xs:p-10 sm:p-14">
+            <strong
+              className="text-black text-4xl sm:text-5xl font-extrabold
+            flex justify-center"
+            >
+              Random Me.
+            </strong>
+            <LoginForm />
+            <LoginSocialMedia />
+            <ContinueAsGuest />
+          </Glass>
+        </div>
+      </PageBackground>
+    </>
   )
 }
