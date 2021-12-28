784a3a7e29ee1c88fc9db37aa626bdabafda7d84
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-28 18:58:41 +0700

    💄 reduce <Glass>'s opacity

--- components/common/Glass.tsx
+++ components/common/Glass.tsx
@@ -6,7 +6,7 @@ export type GlassProps = {
 export default function Glass({ children, className }: GlassProps) {
   return (
     <div
-      className={`bg-gradient-to-br from-white/40 to-white/30 p-12 
+      className={`bg-gradient-to-br from-white/10 to-white/5 p-12 
       backdrop-blur-sm 
       shadow-md hover:shadow-lg
       rounded-xl
