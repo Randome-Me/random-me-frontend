4b7bfab1562badb1ac8978e26191b69d63d9044c
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-28 13:46:36 +0700

    🔧 💄 apply styles from design system

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Button from "components/button"
 
 export default function Home() {
   return (
-    <div className="bg-teal-500 min-w-screen min-h-screen">
+    <>
       <h1>Yo</h1>
       <Button></Button>
-    </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["pages/**/*.{tsx,jsx,js,ts}", "components/**/*.{tsx,jsx,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: {
+          600: "#009CAD",
+          500: "#70DEE6",
+          400: "#009CAD",
+        },
+        yellow: "#FEE551",
+        "black-gray": "#27272A",
+        "white-gray": "#F7FAFC",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 }
