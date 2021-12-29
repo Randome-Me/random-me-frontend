d860f0678b46551258c18a1565366fed9fe99645
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-29 12:25:54 +0700

    💄 increase <Glass> opacity

--- components/pages/login/ContinueAsGuest.tsx
+++ components/pages/login/ContinueAsGuest.tsx
@@ -1,12 +1,13 @@
 import { Icon } from "@iconify/react"
+import Link from "next/link"
 
 export default function ContinueAsGuest() {
   return (
     <span className="block text-center">
       or{" "}
-      <a href="#" className="clickable-text-cyan mr-2">
-        continue as guest
-      </a>
+      <Link href="/">
+        <a className="clickable-text-cyan mr-2">continue as guest</a>
+      </Link>
       <a href="#">
         <Icon
           icon="bi:info-circle-fill"
