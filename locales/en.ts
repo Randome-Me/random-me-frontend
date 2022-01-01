b69f8074c89fed54e82333110253cdad52258a7a
Arm <69001162+arskiir@users.noreply.github.com>
2022-01-01 21:33:25 +0700

    🌐 internationalize /account

--- locales/en.ts
+++ locales/en.ts
@@ -1,6 +1,7 @@
 const en = {
   translation: {
     home: {
+      title: "Random",
       description: "Let's randomize your life decisions with Random Me!",
       topics: "Topics",
       "add option": "Add Option",
@@ -8,6 +9,11 @@ const en = {
       "see probabilities": "See probabilities",
       probability: "Probability",
       option: "Option",
+    },
+    account: {
+      title: "Account",
+      description: "This is where you can edit your account information?",
+      resetPassword: "Reset Password",
     },
   },
 }
--- locales/th.ts
+++ locales/th.ts
@@ -1,6 +1,7 @@
 const th = {
   translation: {
     home: {
+      title: "สุ่ม",
       description: "มาสุ่มชะตาชีวิตของเราด้วย Random Me กันเถอะ",
       topics: "หัวข้อ",
       "add option": "เพิ่มตัวเลือก",
@@ -8,6 +9,11 @@ const th = {
       "see probabilities": "ดูความน่าจะเป็น",
       probability: "ความน่าจะเป็น",
       option: "ตัวเลือก",
+    },
+    account: {
+      title: "บัญชี",
+      description: "คุณสามารถสร้างบัญชีได้ที่นี่",
+      resetPassword: "รีเซ็ตรหัสผ่าน",
     },
   },
 }
