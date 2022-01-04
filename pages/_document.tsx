56ed89ded6c1e00130e389ed226352dc07c936e8
Arm <69001162+arskiir@users.noreply.github.com>
2022-01-04 20:13:39 +0700

    🍱 favicon and og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,400;0,500;0,600;1,400&family=Sen:wght@700;800&family=Kanit:wght@700&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.png" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/og-image.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="/og-image.png" />
       </Head>
       <body>
         <Main />
