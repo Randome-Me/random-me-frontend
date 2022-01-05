42616bfb82548dde498a02660f04fd57240d529c
Arm <69001162+arskiir@users.noreply.github.com>
2022-01-05 13:18:50 +0700

    ♻️ refactor

--- utils/index.ts
+++ utils/index.ts
@@ -151,17 +151,17 @@ export const getProbabilities = () => {
 
 export const randomMe = async () => {
   const {
-    user: { selectedTopicId, _id: userId },
+    user: { selectedTopicId },
   } = store.getState()
   const { arms, probabilityOfEveryArm } = getArmsWithProbabilities()
   const selectedArm = choice(arms, probabilityOfEveryArm)
   const reward = Number(
     confirm(translate("utils.randomConfirm", { option: selectedArm.name }))
   ) as 0 | 1
-  selectedArm.pull(reward ? 1 : 0)
-  if (userId !== guestUserId) {
-    await pullDB(selectedTopicId, selectedArm._id, reward)
-  }
+  selectedArm.pull(reward)
+  loggedInUserDo(
+    async () => await pullDB(selectedTopicId, selectedArm._id, reward)
+  )
 }
 
 export const createDefaultTopic = (_id: string, name: string): Topic => {
